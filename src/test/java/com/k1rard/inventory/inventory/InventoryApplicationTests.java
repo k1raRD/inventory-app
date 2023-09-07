f7ba114b3b09c7269674c1f22c25d8ff2bd18c41
Alex collado <Alexanderyt777@gmail.com>
2023-09-07 15:17:26 -0400

    Initial commit inventory app

---
+++ src/test/java/com/k1rard/inventory/inventory/InventoryApplicationTests.java
@@ -0,0 +1,13 @@
+package com.k1rard.inventory.inventory;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class InventoryApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
